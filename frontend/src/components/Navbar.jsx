8cd424b54955a865f9a5d1b863d6fd4d0caa9ce0
Pelumi Oluwategbe <pelumiayomikunoluwategbe@gmail.com>
2026-03-31 14:25:27 -0500

    Revert "Added login button"
    This reverts commit 0452e8585bb16d029640faa635715580a3ca3235.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,25 +1,24 @@
-import { Link } from "react-router-dom";
-import "../styles/navbar.css";
+import { Link } from 'react-router-dom'
+import '../styles/navbar.css'
 
 function Navbar() {
-	return (
-		<header className='navbar'>
-			<div className='navbar-brand'>
-				<div className='brand-logo'>TP</div>
-				<div>
-					<h2>Tutor Pro</h2>
-					<p>Smart Peer-to-Peer Academic Support</p>
-				</div>
-			</div>
+  return (
+    <header className="navbar">
+      <div className="navbar-brand">
+        <div className="brand-logo">TP</div>
+        <div>
+          <h2>Tutor Pro</h2>
+          <p>Smart Peer-to-Peer Academic Support</p>
+        </div>
+      </div>
 
-			<nav className='navbar-links'>
-				<Link to='/'>Home</Link>
-				<Link to='/signin'>Sign In</Link>
-				<Link to='/signup'>Sign Up</Link>
-				<Link to='/signup'>Log in </Link>
-			</nav>
-		</header>
-	);
+      <nav className="navbar-links">
+        <Link to="/">Home</Link>
+        <Link to="/signin">Sign In</Link>
+        <Link to="/signup">Sign Up</Link>
+      </nav>
+    </header>
+  )
 }
 
-export default Navbar;
+export default Navbar
